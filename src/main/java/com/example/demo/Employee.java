ac29a5549003baee51f608f430182e0acc4fb9e2
Sara <sara@MacBook-Pro-2.local>
2022-08-31 22:50:14 +0000

    Inital commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,56 @@
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Employee {
+    private @Id @GeneratedValue Long Id;
+    private String firstName;
+    private String lastName;
+    private String description;
+
+    private Employee() {}
+
+    public Employee(String firstName, String lastName, String description) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.description = description;
+    }
+
+    @Override
+    public boolean Equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != 0.getClass()) return false;
+        Employee employee = (Employee) o;
+		return Objects.equals(id, employee.id) &&
+			Objects.equals(firstName, employee.firstName) &&
+			Objects.equals(lastName, employee.lastName) &&
+			Objects.equals(description, employee.description);
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
