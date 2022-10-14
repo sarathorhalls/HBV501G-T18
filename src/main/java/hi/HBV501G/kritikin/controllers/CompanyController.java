ab9cbf3c45605aa375c8bd4a8957f767db2be3cb
Sara Þórhallsdóttir <sarathorhalls@gmail.com>
2022-10-14 21:27:42 +0000

    Added documentation

--- src/main/java/hi/HBV501G/kritikin/controllers/CompanyController.java
+++ src/main/java/hi/HBV501G/kritikin/controllers/CompanyController.java
@@ -18,16 +18,47 @@ public class CompanyController {
 
     private CompanyService companyService;
 
+    /**
+     * Constructor for the CompanyController which uses Autowired to inject the
+     * CompanyService from JPA
+     * 
+     * @param companyService
+     */
     @Autowired
     public CompanyController(CompanyService companyService) {
         this.companyService = companyService;
     }
 
+    /**
+     * Returns a list of all companies as a json object from a get request to
+     * /api/companies .
+     * 
+     * @return a list of all companies
+     */
+    @GetMapping(value = HomeController.APIURL + "/companies")
+    public List<Company> fetchAllcompanies() {
+        return companyService.findAll();
+    }
+
+    /**
+     * Returns a particular company as a json object from a get request to
+     * /api/companies/{id} which has the id of the company in the path.
+     * 
+     * @param id the id of the company
+     * @return a particular company
+     */
     @GetMapping(value = HomeController.APIURL + "/company/{id}")
     public Company company(@PathVariable long id) {
         return companyService.findById(id);
     }
 
+    /**
+     * Deletes a particular company from a delete request to /api/companies/{id}
+     * which has the id of the company in the path.
+     * 
+     * @param id the id of the company to be deleted
+     * @return the deleted company
+     */
     @DeleteMapping(value = HomeController.APIURL + "/company/{id}")
     public Company deleteCompany(@PathVariable long id) {
         Company company = companyService.findById(id);
@@ -38,11 +69,14 @@ public class CompanyController {
         return company;
     }
 
-    @GetMapping(value = HomeController.APIURL + "/companies")
-    public List<Company> fetchAllcompanies() {
-        return companyService.findAll();
-    }
-
+    /**
+     * Adds a company to the database from a post request to /api/companies with the
+     * company to be inserted in the body.
+     * 
+     * @param company the company to be inserted, fetched from the body of the post
+     *                request
+     * @return the inserted company or null if the company already exists
+     */
     @PostMapping(value = HomeController.APIURL + "/companies")
     public Company addCompany(@RequestBody Company company) {
         if (company == null || company.getName() == null || company.getName().equals("")) {
