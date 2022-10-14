045000234ee1f2d95c276bd1f70d20a343a01fe0
sarathorhalls <sarathorhalls@gmail.com>
2022-10-14 16:24:39 +0000

    Made CompanyController into RestController

--- src/main/java/hi/HBV501G/kritikin/controllers/CompanyController.java
+++ src/main/java/hi/HBV501G/kritikin/controllers/CompanyController.java
@@ -3,19 +3,17 @@ package hi.HBV501G.kritikin.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import hi.HBV501G.kritikin.persistence.entites.Company;
 import hi.HBV501G.kritikin.services.CompanyService;
 
-@Controller
+@RestController
 public class CompanyController {
 
     private CompanyService companyService;
@@ -26,12 +24,12 @@ public class CompanyController {
     }
 
     @GetMapping(value = HomeController.APIURL + "/company/{id}")
-    public @ResponseBody Company company(@PathVariable long id) {
+    public Company company(@PathVariable long id) {
         return companyService.findById(id);
     }
 
     @DeleteMapping(value = HomeController.APIURL + "/company/{id}")
-    public @ResponseBody Company deleteCompany(@PathVariable long id) {
+    public Company deleteCompany(@PathVariable long id) {
         Company company = companyService.findById(id);
         if (company == null) {
             return null;
@@ -41,12 +39,12 @@ public class CompanyController {
     }
 
     @GetMapping(value = HomeController.APIURL + "/companies")
-    public @ResponseBody List<Company> companies() {
+    public List<Company> fetchAllcompanies() {
         return companyService.findAll();
     }
 
     @PostMapping(value = HomeController.APIURL + "/companies")
-    public @ResponseBody Company addCompany(@RequestBody Company company) {
+    public Company addCompany(@RequestBody Company company) {
         if (company == null || company.getName() == null || company.getName().equals("")) {
             return null;
         }
