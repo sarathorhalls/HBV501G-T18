2d299e8f4459b3c49ac0980f3e66e19d5423a750
Sara Þórhallsdóttir <sarathorhalls@gmail.com>
2022-10-14 21:14:22 +0000

    Added documentation

--- src/main/java/hi/HBV501G/kritikin/controllers/HomeController.java
+++ src/main/java/hi/HBV501G/kritikin/controllers/HomeController.java
@@ -6,8 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    /**
+     * The base URL for all API calls
+     */
     public static final String APIURL = "/api";
 
+    /**
+     * Redirects from / and /company/* to the index page where the React app is
+     * loaded and routes are handled
+     * 
+     * @return the index page
+     */
     @RequestMapping(value = { "/", "/company/*" })
     public String index() {
         return "index";
