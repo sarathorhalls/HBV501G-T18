069b67bda45156e1636aa63d47ae6d3d88b31603
Sara Þórhallsdóttir <sarathorhalls@gmail.com>
2022-10-17 00:25:34 +0000

    Added implemtation for first GET and POST Methods

---
+++ src/main/java/hi/HBV501G/kritikin/controllers/QuestionController.java
@@ -0,0 +1,40 @@
+package hi.HBV501G.kritikin.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import hi.HBV501G.kritikin.persistence.entites.Question;
+import hi.HBV501G.kritikin.services.QuestionService;
+
+@RestController
+public class QuestionController {
+    QuestionService questionService;
+
+    @Autowired
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @GetMapping(value = HomeController.APIURL + "/company/{id}/questions")
+    public List<Question> fetchQuestionsByCompany(@PathVariable long id) {
+        return questionService.findByCompany(id);
+    }
+
+    @GetMapping(value = HomeController.APIURL + "/user/{id}/questions")
+    public List<Question> fetchQuestionsByUser(@PathVariable long id) {
+        return questionService.findByUser(id);
+    }
+
+    @PostMapping(value = HomeController.APIURL + "/company/{id}/questions")
+    public Question addQuestion(@RequestBody Question question, @RequestParam long userId,
+            @PathVariable long companyId) {
+        return questionService.addQuestion(question, userId, companyId);
+    }
+}
