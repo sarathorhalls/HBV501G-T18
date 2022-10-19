3178281ea3ddfbf072486ca577a42c53c3e1a7cc
Reyn Alpha <reynalpha@outlook.com>
2022-10-19 14:41:32 +0000

    Add unique key to each star

--- src/main/js/components/rating.js
+++ src/main/js/components/rating.js
@@ -8,25 +8,25 @@ export default function StarRating(props) {
     // Array of star icons
     const stars = [];
 
+    // Number of stars in total
+    const noOfTotalStars = 5;
     // Number of completely filled star icons
-    const filledStars = Math.floor(props.rating);
+    const noOfFilledStars = Math.floor(props.rating);
     // Add appropriate number of filled star icons
-    for (let i = 0; i < filledStars; i++) {
-        stars.push(<StarIcon />);
+    let i = 0;
+    for (; i < noOfFilledStars; i++) {
+        stars.push(<StarIcon key={i} />);
     }
 
-    // Number of empty star icons
-    let emptyStars = 5 - filledStars;
-    
     // If star rating is not an integer, add half a star
-    if (props.rating !== filledStars) {
-        stars.push(<StarHalfIcon />);
-        emptyStars--;  // Decrease number of empty stars by one
+    if (props.rating !== noOfFilledStars) {
+        stars.push(<StarHalfIcon key={i} />);
+        i++;  // Decrease number of remaining stars by one
     }
 
     // Add remaining empty stars
-    for (let i = 0; i < emptyStars; i++) {
-        stars.push(<StarOutlineIcon />);
+    for (; i < noOfTotalStars; i++) {
+        stars.push(<StarOutlineIcon key={i} />);
     }
 
     // TODO: add accessible way of reading rating
