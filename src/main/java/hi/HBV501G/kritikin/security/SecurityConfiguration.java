ff7e60c097799ac1de8b55b9940c4f41d498d2fd
Sara Þórhallsdóttir <sarathorhalls@gmail.com>
2022-10-27 08:30:00 +0000

    Changed implementation of AuthenticationManager

--- src/main/java/hi/HBV501G/kritikin/security/SecurityConfiguration.java
+++ src/main/java/hi/HBV501G/kritikin/security/SecurityConfiguration.java
@@ -2,6 +2,8 @@ package hi.HBV501G.kritikin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,11 +14,18 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().anyRequest().permitAll()
                 .and().httpBasic()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().addFilter(new CustomAuthenticationFilter(new CustomAuthenticationManager()));
+                .and().addFilter(new CustomAuthenticationFilter(
+                        authenticationManager(http.getSharedObject(AuthenticationConfiguration.class))));
         return http.build();
     }
 }
