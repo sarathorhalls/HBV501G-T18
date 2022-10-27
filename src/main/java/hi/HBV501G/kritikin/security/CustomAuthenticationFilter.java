ea54d27ab85c96dc291d078195ae064e15ea351c
Sara Þórhallsdóttir <sarathorhalls@gmail.com>
2022-10-27 08:45:23 +0000

    moved access and refresh tokens to body

--- src/main/java/hi/HBV501G/kritikin/security/CustomAuthenticationFilter.java
+++ src/main/java/hi/HBV501G/kritikin/security/CustomAuthenticationFilter.java
@@ -2,6 +2,8 @@ package hi.HBV501G.kritikin.security;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.servlet.FilterChain;
@@ -20,51 +22,60 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    Logger logger = LoggerFactory.getLogger(CustomAuthenticationFilter.class);
+        Logger logger = LoggerFactory.getLogger(CustomAuthenticationFilter.class);
 
-    private final AuthenticationManager authenticationManager;
+        private final AuthenticationManager authenticationManager;
 
-    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
-    }
+        public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+                this.authenticationManager = authenticationManager;
+        }
 
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
-            throws AuthenticationException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        logger.info("attempted authentication with username: {} and password: {}", username, password);
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
-                password);
-        return authenticationManager.authenticate(authenticationToken);
-    }
+        @Override
+        public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+                        throws AuthenticationException {
+                String username = request.getParameter("username");
+                String password = request.getParameter("password");
+                logger.info("attempted authentication with username: {} and password: {}", username, password);
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                                username,
+                                password);
+                return authenticationManager.authenticate(authenticationToken);
+        }
 
-    /**
-     * 
-     */
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
-            Authentication authentication) throws IOException, ServletException {
-        org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) authentication
-                .getPrincipal();
-        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
-        String access_token = JWT.create()
-                .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
-                .withIssuer(request.getRequestURL().toString())
-                .withClaim("authorities",
-                        user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
-                .sign(algorithm);
-        String refresh_token = JWT.create()
-                .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 60 * 60 * 24 * 1000))
-                .withIssuer(request.getRequestURL().toString())
-                .sign(algorithm);
-        response.setHeader("access_token", access_token);
-        response.setHeader("refresh_token", refresh_token);
-    }
+        /**
+         * 
+         */
+        @Override
+        protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                        FilterChain chain,
+                        Authentication authentication) throws IOException, ServletException {
+                org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) authentication
+                                .getPrincipal();
+                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                String access_token = JWT.create()
+                                .withSubject(user.getUsername())
+                                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                                .withIssuer(request.getRequestURL().toString())
+                                .withClaim("authorities",
+                                                user.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+                                                                .collect(Collectors.toList()))
+                                .sign(algorithm);
+                String refresh_token = JWT.create()
+                                .withSubject(user.getUsername())
+                                .withExpiresAt(new Date(System.currentTimeMillis() + 60 * 60 * 24 * 1000))
+                                .withIssuer(request.getRequestURL().toString())
+                                .sign(algorithm);
+                // response.setHeader("access_token", access_token);
+                // response.setHeader("refresh_token", refresh_token);
+                Map<String, String> tokens = new HashMap<>();
+                tokens.put("access_token", access_token);
+                tokens.put("refresh_token", refresh_token);
+                response.setContentType("application/json");
+                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+        }
 
 }
