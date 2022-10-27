855343f9788cfe01f8bc921965c2570ec03dc08d
Sara Þórhallsdóttir <sarathorhalls@gmail.com>
2022-10-27 08:44:45 +0000

    moved the authenticationFilter, changed login url and added documentation

--- src/main/java/hi/HBV501G/kritikin/security/SecurityConfiguration.java
+++ src/main/java/hi/HBV501G/kritikin/security/SecurityConfiguration.java
@@ -13,19 +13,38 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    /**
+     * Allows us to use the AuthenticationManager in our custom filter
+     * 
+     * @param authenticationConfiguration
+     * @return
+     * @throws Exception
+     */
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
             throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    /**
+     * Handles the security of the application through the use of filters
+     * 
+     * @param http
+     * @return
+     * @throws Exception
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Create the authentication filter
+        CustomAuthenticationFilter authenticationFilter = new CustomAuthenticationFilter(
+                authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)));
+        // Set the filter login url to /api/login
+        authenticationFilter.setFilterProcessesUrl("/api/login");
+
         http.csrf().disable().authorizeRequests().anyRequest().permitAll()
                 .and().httpBasic()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().addFilter(new CustomAuthenticationFilter(
-                        authenticationManager(http.getSharedObject(AuthenticationConfiguration.class))));
+                .and().addFilter(authenticationFilter);
         return http.build();
     }
 }
