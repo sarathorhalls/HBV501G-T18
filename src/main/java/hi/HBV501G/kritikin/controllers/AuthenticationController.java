148e25ba36d3129a5ed56d92e34cb5c733c977ff
Sara Þórhallsdóttir <sarathorhalls@gmail.com>
2022-11-10 04:37:47 +0000

    Added GET Request for refreshTokens

--- src/main/java/hi/HBV501G/kritikin/controllers/AuthenticationController.java
+++ src/main/java/hi/HBV501G/kritikin/controllers/AuthenticationController.java
@@ -1,5 +1,9 @@
 package hi.HBV501G.kritikin.controllers;
 
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+
 /**
  * This class is the controller for the authentication of users. It handles the
  * login and registration of users from REST requests.
@@ -9,6 +13,11 @@ package hi.HBV501G.kritikin.controllers;
  */
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,14 +27,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import hi.HBV501G.kritikin.persistence.entites.Authority;
 import hi.HBV501G.kritikin.persistence.entites.User;
 import hi.HBV501G.kritikin.services.CompanyUserService;
 import hi.HBV501G.kritikin.services.UserService;
 
 @RestController
 public class AuthenticationController {
-    // TODO: use actual authentication
-
     private UserService userService;
     private CompanyUserService companyUserService;
 
@@ -87,5 +101,50 @@ public class AuthenticationController {
         }
         userService.delete(user);
         return user;
+    }
+
+    /**
+     * Returns a new access token for the user with the given refreshToken from the
+     * authorization bearer header.
+     * 
+     * @param request
+     * @param response
+     * @throws IOException
+     */
+    @GetMapping(value = HomeController.APIURL + "/refreshToken")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            try {
+                String refreshToken = authorizationHeader.substring("Bearer ".length());
+                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(refreshToken);
+                String username = decodedJWT.getSubject();
+                User user = userService.findByUsername(username);
+                String accessToken = JWT.create()
+                        .withSubject(user.getUsername())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                        .withIssuer(request.getRequestURL().toString())
+                        .withClaim("authorities",
+                                user.getAuthorities().stream().map(Authority::getName).collect(Collectors.toList()))
+                        .sign(algorithm);
+                Map<String, String> tokens = new HashMap<>();
+                tokens.put("access_token", accessToken);
+                tokens.put("refresh_token", refreshToken);
+                response.setContentType("application/json");
+                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+            } catch (Exception e) {
+                response.setHeader("error", e.getMessage());
+                response.setStatus(403);
+                // response.sendError(403);
+                Map<String, String> error = new HashMap<>();
+                error.put("error_message", e.getMessage());
+                response.setContentType("application/json");
+                new ObjectMapper().writeValue(response.getOutputStream(), error);
+            }
+        } else {
+            throw new RuntimeException("Refresh token is missing");
+        }
     }
 }
