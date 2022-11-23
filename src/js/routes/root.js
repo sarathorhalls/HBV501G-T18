0176d8022addbbadc42c471f4fc8303a6d00b20a
Reyn Alpha <reynalpha@outlook.com>
2022-11-23 16:39:39 +0000

    Add logo

--- src/js/routes/root.js
+++ src/js/routes/root.js
@@ -5,6 +5,11 @@ import TextField from "@mui/material/TextField";
 export default function Root(props) {
     return (
         <Container maxWidth="lg">
+            <img
+                id="kritikin_logo"
+                src={"/img/kritikin-logo.svg"}
+                style={{ width: "40em", maxWidth: "80%", display: "block", margin: "0 auto"}}
+            />
             <form
                 id="body_search_form"
                 onSubmit={props.search}
